da68eff63e714cc16434b99720ccdd5b651f705b
louis <louisdx24@icloud.com>
2024-03-26 19:19:54 +0800

    初始化完成todo功能

--- todo/urls.py
+++ todo/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from app import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("user/", include("user.urls")),
-    path("", include("todo.urls")),
+    path("", views.todo, name="todo"),
 ]
