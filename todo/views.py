67284020968ac88e57018d4087ec2416283c7503
louis <louisdx24@icloud.com>
2024-03-28 20:32:51 +0800

    完成建立事項表單

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
+
+
+def create_todo(request):
+    message = ""
+    form = TodoForm()
+    if request.method == "POST":
+        try:
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            message = "建立事項成功"
+
+            return redirect("todolist")
+        except Exception as e:
+            print(e)
+            message = "建立事項失敗..."
+
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
 
 
 # Create your views here.
-def todo(request):
+def todolist(request):
     # all,get,filter
     # todos = Todo.objects.all()
     todos = None
